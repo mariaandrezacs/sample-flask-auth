50c8576b5aef2ad14d1bf2f0a0e00bdfa1c7d3de
Maria Andreza C Santos <mandreza008@gmail.com>
2025-01-03 23:37:20 -0300

    finaliza func login

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, jsonify
 from models.user import User
 from database import db
-from flask_login import LoginManager
+from flask_login import LoginManager, login_user, current_user
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -11,6 +11,13 @@ login_manager = LoginManager()
 db.init_app(app)
 login_manager.init_app(app)
 # view login
+login_manager.login_view = 'login'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 
 @app.route('/login', methods=["POST"])
 def login():
@@ -19,12 +26,14 @@ def login():
     password = data.get("password")
 
     if username and password:
-        # LOGIN
         user = User.query.filter_by(username=username).first()
         if user and user.password == password:
+            login_user(user)
+            print(current_user.is_authenticated)
             return jsonify({"message": "Autenticação realizada com sucesso"})
     
     return jsonify({"message": "Credenciais inválidas."}), 400
+
 
 @app.route('/hello-world', methods=["GET"])
 def hello_world():
