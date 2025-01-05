096f7ba768e2bbf0be45b9049490c27fd963014d
Maria Andreza C Santos <mandreza008@gmail.com>
2025-01-04 23:20:29 -0300

    feat: cria func create_user

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
-from flask import Flask, request, jsonify
-from models.user import User
+from flask import Flask, jsonify, request
+from flask_login import (LoginManager, current_user, login_required,
+                         login_user, logout_user)
+
 from database import db
-from flask_login import LoginManager, login_user, current_user, logout_user, login_required
+from models.user import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "your_secret_key"
@@ -18,7 +20,6 @@ login_manager.login_view = 'login'
 def load_user(user_id):
     return User.query.get(user_id)
 
-
 @app.route('/login', methods=["POST"])
 def login():
     data = request.json
@@ -34,13 +35,25 @@ def login():
     
     return jsonify({"message": "Credenciais inválidas."}), 400
 
-
 @app.route('/logout', methods=["GET"])
 @login_required
 def logout():
     logout_user()
     return jsonify({"message": "Logout realizado com sucesso."})
 
+@app.route('/user', methods=["POST"])
+@login_required
+def create_user():
+    data = request.json
+    username = data.get("username")
+    password = data.get("password")
+
+    if username and password:
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "Usuario cadastrado com sucesso."})
+    return jsonify({"message": "Dados inválidos."}), 400
 
 
 @app.route('/hello-world', methods=["GET"])
