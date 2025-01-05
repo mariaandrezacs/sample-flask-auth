e2f0123bd30a40fcda2656fddc146ebf7034eebf
Maria Andreza C Santos <mandreza008@gmail.com>
2025-01-04 23:26:53 -0300

    feat: func busca usuario

--- app.py
+++ app.py
@@ -56,6 +56,18 @@ def create_user():
     return jsonify({"message": "Dados inválidos."}), 400
 
 
+@app.route('/user/<int:id_user>', methods=["GET"])
+@login_required
+def read_user(id_user): 
+    user = User.query.get(id_user)
+
+    if user: 
+        return {"username": user.username}
+
+    return jsonify({"message": "Usuario não encontrado"}), 404
+
+
+
 @app.route('/hello-world', methods=["GET"])
 def hello_world():
     return "Hello world"
